6a4b0365d4cd595883fb603a6574d97f0f38871f
Don Myers <donmyers@me.com>
2026-03-16 07:05:44 -0400

    Update install script to remove package cloning and update composer.json for local package development.

--- install/install.sh
+++ install/install.sh
@@ -12,16 +12,6 @@ mkdir var/working
 
 chmod 777 var/*
 
-mkdir packages
-
-cd packages
-
-git clone git@github.com:ProjectOrangeBox/OrangePackage.git Orange
-
-git clone https://github.com/ProjectOrangeBox/Peels Peels
-
-cd ..
-
 cp support/samples/sample.env .env
 
 echo "Now run composer install\n";
