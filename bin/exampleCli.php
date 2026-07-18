9be0a8b80476c117bdcf107065388349b90544b2
Don Myers <donmyers@me.com>
2026-07-18 00:07:24 -0400

    updates

--- bin/exampleCli.php
+++ bin/exampleCli.php
@@ -3,26 +3,7 @@
 
 declare(strict_types=1);
 
-use orange\framework\Application;
-
-// All directories are based off of this root path and
-// everything goes under this path for security and easier portability
-define('__ROOT__', realpath(__DIR__ . '/../'));
-
-// where is our public www directory?
-define('__WWW__', __ROOT__ . '/htdocs');
-
-// bootstrap before anything else
-if (file_exists(__ROOT__ . '/bootstrap.php')) {
-    require_once __ROOT__ . '/bootstrap.php';
-}
-
-// load the standard composer autoloader
-require_once __ROOT__ . '/vendor/autoload.php';
-
-$cliApplication = Application::make([__ROOT__ . '/.env'], [__ROOT__ . '/config']);
-
-$container = $cliApplication->run();
+$container = require __DIR__ . '/../bootstrapCli.php';
 
 // start shellscript
 $console = $container->console;
---
+++ bootstrapCli.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use orange\framework\Application;
+
+// All directories are based off of this root path and
+// everything goes under this path for security and easier portability
+define('__ROOT__', realpath(__DIR__));
+
+// where is our public www directory?
+define('__WWW__', __ROOT__ . '/htdocs');
+
+// bootstrap before anything else
+if (file_exists(__ROOT__ . '/bootstrap.php')) {
+    require_once __ROOT__ . '/bootstrap.php';
+}
+
+// load the standard composer autoloader
+require_once __ROOT__ . '/vendor/autoload.php';
+
+// and away we go! CLI scripts (bin/*, vendor/*/bin/*) do:
+//   $container = require __ROOT__ . '/bootstrapCli.php';
+return Application::make([__ROOT__ . '/.env'], [__ROOT__ . '/config'])->run();
